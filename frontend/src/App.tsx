2e5cf47b56cb5a2c06b7e300fca244c6773c0367
venfar <farlenkovveniamin@gmail.com>
2026-02-25 19:34:15 +0500

    Инициализация клиентской части

---
+++ frontend/src/App.tsx
@@ -0,0 +1,45 @@
+import './App.css'
+
+function App() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Дата</th>
+          <th>ФИО</th>
+          <th>Объект</th>
+          <th>Телефон</th>
+          <th>Email</th>
+          <th>Заводские номера</th>
+          <th>Тип приборов</th>
+          <th>Эмоциональный окрас</th>
+          <th>Суть вопроса</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+          </td>
+          <td>
+          </td>
+          <td>
+          </td>
+          <td>
+          </td>
+          <td>
+          </td>
+          <td>
+          </td>
+          <td>
+          </td>
+          <td>
+          </td>
+          <td>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+export default App
