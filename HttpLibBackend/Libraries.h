e37a6303b4cfa9feaf2c1b33abef667a9e67e208
fr1zb3e <168219806+Frolotey1@users.noreply.github.com>
2026-02-25 20:41:36 +0500

    Инициализация backend части

---
+++ HttpLibBackend/Libraries.h
@@ -0,0 +1,7 @@
+#ifndef LIBRARIES_H
+#define LIBRARIES_H
+#include <iostream>
+#include "httplib.h"
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+#endif // LIBRARIES_H
---
+++ HttpLibBackend/build/Desktop-Debug/CMakeFiles/HttpLibBackend.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for HttpLibBackend.
+# This may be replaced when dependencies are built.
